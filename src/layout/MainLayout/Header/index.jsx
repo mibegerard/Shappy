207fdb0134cbf6d10a44cea1e8ad8b4a811a4e16
Mibe Keumeni Abba Gerard <mibekeumeni@icloud.com>
2024-07-24 02:16:45 +0200

    SI-18 scroll header and responsive footer

--- src/layout/MainLayout/Header/index.jsx
+++ src/layout/MainLayout/Header/index.jsx
@@ -15,13 +15,28 @@ import { useTheme } from '@mui/material/styles';
 import logoSrc from 'assets/images/logobelge.png';
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
+import AgricultureIcon from '@mui/icons-material/Agriculture';
 import { Link } from 'react-router-dom';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
 
 const menuItems = [
-    { name: 'Je suis Producteur', link: '/', icon: <HomeIcon color="primary" /> }
+    { name: 'Je suis Producteur', link: '/', icon: <AgricultureIcon color="primary" /> },
+    { name: 'Se Connecter', link: '/', icon: <HomeIcon color="primary" /> }
 ];
 
-const Header = () => {
+const HideOnScroll = (props) => {
+    const { children } = props;
+    const trigger = useScrollTrigger();
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+};
+
+const Header = (props) => {
     const theme = useTheme();
     const isLargeScreen = useMediaQuery(theme.breakpoints.up('md'));
     const [drawerOpen, setDrawerOpen] = useState(false);
@@ -36,13 +51,14 @@ const Header = () => {
     const drawerContent = (
         <Box
             sx={{
-                width: 150
+                width: '30vw', // 30% of the screen width
+                maxWidth: 300, // Maximum width
             }}
             role="presentation"
             onClick={toggleDrawer(false)}
             onKeyDown={toggleDrawer(false)}
         >
-            <List color="white">
+            <List>
                 {menuItems.map((item, index) => (
                     <ListItem button key={item.name + index}>
                         <ListItemText>
@@ -64,58 +80,57 @@ const Header = () => {
 
     return (
         <>
-            <AppBar position="fixed" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
-                <Container maxWidth="lg">
-                    <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
-                        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                            <Button component={Link} to="/" sx={{ p: 0 }}>
-                                <img
-                                    src={logoSrc}
-                                    alt="Logo"
-                                    style={{
-                                        maxHeight: '30px',
-                                        width: 'auto',
-                                        height: '30px'
-                                    }}
-                                />
-                            </Button>
-                        </Box>
-                        {isLargeScreen ? (
+            <HideOnScroll {...props}>
+                <AppBar position="fixed" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+                    <Container maxWidth="lg">
+                        <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
                             <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-                                    {menuItems.map((item, index) => (
-                                        <Box
-                                            key={item.name + index}
-                                            component={Link}
-                                            to={item.link}
-                                            sx={{
-                                                textDecoration: 'none',
-                                                backgroundColor: '#F5F5DC',
-                                                padding: '0.5rem 1rem',
-                                                marginRight: '1rem',
-                                                borderRadius: '10px',
-                                                display: 'flex',
-                                                alignItems: 'center',
-                                            }}                                            
-                                        >
-                                            <Typography variant="h6" sx={{ color: '#FC8A1A' }}>
-                                                {item.name}
-                                            </Typography>
-                                        </Box>
-                                    ))}
-                                </Box>
-                                <Button sx={{ color: 'white' }} size="large" component="a" href="#">
-                                    Se connecter
+                                <Button component={Link} to="/" sx={{ p: 0 }}>
+                                    <img
+                                        src={logoSrc}
+                                        alt="Logo"
+                                        style={{
+                                            maxHeight: '30px',
+                                            width: 'auto',
+                                            height: '30px'
+                                        }}
+                                    />
                                 </Button>
                             </Box>
-                        ) : (
-                            <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
-                                <MenuIcon />
-                            </IconButton>
-                        )}
-                    </Toolbar>
-                </Container>
-            </AppBar>
+                            {isLargeScreen ? (
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                                        {menuItems.map((item, index) => (
+                                            <Box
+                                                key={item.name + index}
+                                                component={Link}
+                                                to={item.link}
+                                                sx={{
+                                                    textDecoration: 'none',
+                                                    backgroundColor: item.name === 'Se Connecter' ? 'transparent' : '#F5F5DC',
+                                                    padding: '0.5rem 1rem',
+                                                    marginRight: '1rem',
+                                                    borderRadius: '10px',
+                                                    display: 'flex',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                <Typography variant="h6" sx={{ color: item.name === 'Se Connecter' ? '#FFF4E2' : '#FC8A1A' }}>
+                                                    {item.name}
+                                                </Typography>
+                                            </Box>
+                                        ))}
+                                    </Box>
+                                </Box>
+                            ) : (
+                                <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+                                    <MenuIcon />
+                                </IconButton>
+                            )}
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </HideOnScroll>
             {!isLargeScreen && (
                 <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
                     {drawerContent}
