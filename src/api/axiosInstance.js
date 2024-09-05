6c92898aac34bbedc52065f43f6035551eeb4d09
Mibe Keumeni Abba Gerard <mibekeumeni@icloud.com>
2024-09-05 14:12:58 +0200

    SI-149 front end deploy

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: process.env.VITE_APP_END_POINT, 
 });
 
 // Request interceptor
